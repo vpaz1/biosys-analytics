dee5f1f1f7d6ebf7a20042caab6a9d19a163aabd
Veronica Paz <vpaz@email.arizona.edu>
2019-01-29 09:25:36 -0700

    homework 2

--- assignments/02-bash-scripting/cat-n.sh
+++ assignments/02-bash-scripting/cat-n.sh
@@ -4,17 +4,16 @@
 
 FILE=$1 # first argument
 
-if [[ $# -ne 1 ]]; then # if the num of args is equal to 1
+if [[ $# -eq 0 ]]; then # if the num of args is equal to 1
 	echo "Usage: cat-n.sh FILE"
 	exit 1
-elif [[ $# -eq 1 ]] && [[ -f "$FILE" ]]; then
+elif [[ ! -f "$FILE" ]]; then
+	echo "foo is not a file"
+        exit 1 
+
+fi
 i=0
 while read -r LINE; do
-	i=$((i+1))
-	echo $i $LINE
+        i=$((i+1))
+        echo "$i" "$LINE"
 done < "$FILE"
-	exit 1
-else
-	echo "$FILE is not a file"
-        exit 1
-fi
