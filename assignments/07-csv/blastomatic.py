129ecd4aa1e20d6e1ca2935713056be195bd414a
Veronica Paz <vpaz@email.arizona.edu>
2019-03-14 08:43:26 -0700

    homework 7

---
+++ assignments/07-csv/blastomatic.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""
+Author : vpaz
+Date   : 2019-03-01
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import sys
+import os
+import csv
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Annotate BLAST output',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+   
+   # parser.add_argument(
+    #    'input', nargs='+', type=str)
+
+    parser.add_argument(
+        'FILE', nargs='+',  metavar='FILE', help='BLAST output (-outfmt 6)')
+
+    parser.add_argument(
+        '-a',
+        '--annotations',
+        help='Annotation file',
+        metavar='FILE',
+        type=str,
+        default='')
+
+    parser.add_argument(
+        '-o',
+        '--outfile',
+        help='Output file',
+        metavar='FILE',
+        type=str,
+        default='')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    annot = args.annotations
+    file_name = args.FILE
+    out_file = args.outfile
+
+    print(file_name[0])
+    print(annot)
+    
+    tab_file = file_name[0] 
+    if not os.path.isfile(tab_file):
+        die('"{}" is not a file'.format(tab_file))
+    
+    
+    with open(annot, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter ='\t')
+    #    for row_csv in reader:
+     #      print('i')
+   
+    with open(tab_file, 'r') as tabfile:
+        readers = csv.reader(tabfile, delimiter = '\t')
+        print('seq_id\t')
+        for rows in readers:
+           # row = list(rows)
+            seq_id = rows[1][0:]
+            pident = rows[2][0:] 
+            print(seq_id)
+            print(pident)
+          
+           #outfile.write('\t',join(seq_id, pident)
+        
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
