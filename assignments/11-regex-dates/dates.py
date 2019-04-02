af853cebc4a7e96a7a8b8eca43ee76c04d706499
Veronica Paz <vpaz@email.arizona.edu>
2019-04-02 10:54:35 -0700

    homework 11

--- assignments/11-regex-dates/dates.py
+++ assignments/11-regex-dates/dates.py
@@ -19,24 +19,24 @@ def main():
         sys.exit(1)
  
     
-    month_dict = dict(jan='01', feb='02', mar='03', apr='04', may='05', jun='06', jul='07', aug='08', sep='09',
-                  oct='10', nov='11', dec='12')
+    month_dict = {'jan':'01', 'feb':'02', 'mar':'03', 'apr':'04', 'may':'05', 'jun':'06', 'jul':'07', 'aug':'08', 'sep':'09','oct':'10', 'nov':'11', 'dec':'12'}
     
     for date in args:
         date_one = re.compile(
                        '(?P<year>\d{4})'
-                       '[-]?''\s*'
-                       '(?P<month>\d{2})'
-                       '[-]?''\s*'
-                       '(?P<day>\d{2})')
+                       '[-/,]'
+                       '(?P<month>\d{1,2})'
+                       '[-/,]'
+                       '(?P<day>\d{1,2})'
+                       '[$|\D]')
 
         date_two = re.compile('(?P<year>\d{4})'
-                       '[-]?'
-                       '(?P<month>\d{2})'
-                       '(?P<day>\d{0})')
+                       '[-/,]'
+                       '(?P<month>\d{1,2})'
+                       )
 
-        date_three = re.compile('(?P<month>\d{2})'
-                       '[/]?'
+        date_three = re.compile('(?P<month>\d{1,2})'
+                       '[-/,]'
                        '(?P<year>\d{2})'
                        '(?P<day>\d{0})')
         
@@ -47,90 +47,46 @@ def main():
                              '(?P<day>\d{0})')
         date_five = re.compile(
                        '(?P<year>\d{4})'
-                       '[-]?''\s*'
-                       '(?P<month>\d{1})'
-                       '[-]?''\s*'
-                       '(?P<day>\d{1})')
-
-        date_six = re.compile('(?P<year>\d{4})'
-                       '[-]?''\s*'
-                       '(?P<month>\d{1})'
-                       '(?P<day>\d{0})')
-
-        date_seven = re.compile('(?P<month>\d{1})'
-                       '[/-]?''\s*'
-                       '(?P<year>\d{2})'
-                       '(?P<day>\d{0})')
-        
-        date_eight = re.compile(
-                       '(?P<year>\d{2})'
-                       '[/-]?'
-                       '(?P<month>\d{0})'
                        
-                       '(?P<day>\d{0})')
-         
-        #date_nine = re.compile('(?P<month>\d{1})'
-         #              '[/]?''\s*'
-          #             '(?P<year>\d{4})'
+                       '(?P<month>\d{1,2})'
+                       
+                       '(?P<day>\d{1,2})')
+        #date_six = re.compile('(?P<month>\d{1,2})'
+         #              '[-/,]'
+          #             '(?P<year>\d{2})'
            #            '(?P<day>\d{0})')
-   # for date in args:
-        if date_one.match(date): #or date_two.match(date) or date_three.match(date)
-            match = date_one.match(date)
+
+
+        match = date_one.match(date) or date_two.match(date) or date_three.match(date) or date_three.match(date) or date_four.match(date) or date_five.match(date)
+        if match:
+            match_dict = match.groupdict()
+           # print(match_dict)
+            day = match_dict.get('day')
+            month = match_dict.get('month')
+           # print(month)
+            year = match_dict.get('year') 
+      # print(month)
+            if day:
+               if len(day) == 1:
+                  day = '0'+ day
+            else:
+               day = '01'
+            if len(month) == 1:
+               month = '0'+ month
+             #  print(month) 
+            if len(month) >= 3:
+               month = month_dict[month.lower()[:3]]
+              # print(month)
+            if len(year) == 2:
+               year = '20' + year
             tmpl = '{year}-{month}-{day}'
-            standard = tmpl.format(year=match.group('year'), 
-                               month=match.group('month'),
-                               day=match.group('day'))
-        elif date_two.match(date):
-            match = date_two.match(date)
-            tmpl = '{year}-{month}-01'
-            standard = tmpl.format(year=match.group('year'),
-                               month=match.group('month'),
-                               day=match.group('day'))
-        elif date_three.match(date):
-            match = date_three.match(date)
-            tmpl = '20{year}-{month}-01'
-            standard = tmpl.format(year=match.group('year'),
-                               month=match.group('month'),
-                               day=match.group('day'))
-        elif date_four.match(date):
-            match = date_four.match(date)
-            tmpl = '{year}-{month}-01'
-            standard = tmpl.format(year=match.group('year'),
-                               month=month_dict[match.group('month').lower()[:3]],
-                               day=match.group('day'))
-        elif date_five.match(date):
-            match = date_five.match(date)
-            tmpl = '{year}-0{month}-0{day}'
-            standard = tmpl.format(year=match.group('year'),
-                               month=match.group('month'),
-                               day=match.group('day'))
-        elif date_six.match(date):
-            match = date_six.match(date)
-            tmpl = '{year}-0{month}-01'
-            standard = tmpl.format(year=match.group('year'),
-                               month=match.group('month'),
-                               day=match.group('day'))
-        elif date_seven.match(date):
-            match = date_seven.match(date)
-            tmpl = '20{year}-0{month}-01'
-            standard = tmpl.format(year=match.group('year'),
-                               month=match.group('month'),
-                               day=match.group('day'))
-        elif date_eight.match(date):
-            match = date_eight.match(date)
-            tmpl = '{year}-01-01'
-            standard = tmpl.format(year=match.group('year'),
-                               month=match.group('month'),
-                               day=match.group('day'))
-       # elif date_nine.match(date):
-        #    match = date_nine.match(date)
-         #   tmpl = '{year}-0{month}-01'
-          #  standard = tmpl.format(year=match.group('year'),
-           #                    month=match.group('month'),
-            #                   day=match.group('day'))
+           # standard = tmpl.format(year, 
+            #                   month,
+             #                  day)
+            print('{}-{}-{}'.format(year, month, day))
         else:
             print('No match')
 
-        print('{}'.format(standard))
+       # print('{}'.format(standard))
 # --------------------------------------------------
 main()
